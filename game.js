92f7bc5c164ca716f5680205605e677736088841
Ali Naqvi <ali.naqvi8062@gmail.com>
2023-06-14 17:16:20 -0400

    Create a function that plays one round of the game and returns the winner

--- game.js
+++ game.js
@@ -19,5 +19,43 @@ function getComputerChoice() {
 
 let computerSelection = getComputerChoice();
 let playerSelection = prompt("Rock, Paper or Scissors?!");
-console.log(computerSelection);
-console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+
+    let x = playerSelection.toUpperCase();
+    let y = computerSelection.toUpperCase(); // make both selections uppercase to make it case insensitive.
+
+    if (x === y){ //begin by checking for a tie.
+        return `The round is a tie! Both players chose ${x}`
+    }
+
+    if (x === 'ROCK'){
+        if (y === 'PAPER'){
+            return `You Lose! CPU chose ${y} and you chose ${x}`
+        }
+        else if (y === 'SCISSORS'){
+            return `You win! CPU chose ${y} and you chose ${x}`
+        }
+    }
+
+    else if (x === 'PAPER'){
+        if (y === 'SCISSORS'){
+            return `You Lose! CPU chose ${y} and you chose ${x}`
+        }
+        else if (y === 'ROCK'){
+            return `You win! CPU chose ${y} and you chose ${x}`
+        }
+    }
+
+    else if (x === 'SCISSORS'){
+        if (y === 'ROCK'){
+            return `You Lose! CPU chose ${y} and you chose ${x}`
+        }
+        else if (y === 'PAPER'){
+            return `You win! CPU chose ${y} and you chose ${x}`
+        }
+    }
+  
+}
+
+console.log(playRound(playerSelection, computerSelection));
