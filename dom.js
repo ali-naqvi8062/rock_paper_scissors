4c299e09b08178c34b021d32d10f4ad6947bad29
Ali Naqvi <ali.naqvi8062@gmail.com>
2023-07-02 00:58:45 -0400

    Create UI for when game begins by manipulating the DOM

--- dom.js
+++ dom.js
@@ -1,7 +1,28 @@
 const btn = document.querySelector('.play');
 
 btn.addEventListener('click', function (e) {
-    setTimeout(function(){
-        btn.style.display = 'none';; //This will be delayed for one second
-    }, 300);
+
+    btn.remove()
+
+    const gameBox = document.querySelector('.game');
+
+    const btnRock = document.createElement('button');
+    const btnPaper = document.createElement('button');
+    const btnScissors = document.createElement('button');
+
+    btnRock.classList.add('bux');
+    btnPaper.classList.add('bux');
+    btnScissors.classList.add('bux');
+
+    btnRock.textContent = 'Rock';
+    btnPaper.textContent = 'Paper';
+    btnScissors.textContent = 'Scissors';
+
+    gameBox.appendChild(btnRock);
+    gameBox.appendChild(btnPaper);
+    gameBox.appendChild(btnScissors);
+
+
   });
+
+  
