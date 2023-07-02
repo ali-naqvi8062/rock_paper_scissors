aa4e7d16b45e8a9e4ae73ceaf4b173523579333f
Ali Naqvi <ali.naqvi8062@gmail.com>
2023-07-02 00:11:21 -0400

    Add event listener for play button

--- dom.js
+++ dom.js
@@ -1,2 +1,7 @@
-const meow = document.querySelector('play');
-console.log(meow);
+const btn = document.querySelector('.play');
+
+btn.addEventListener('click', function (e) {
+    setTimeout(function(){
+        btn.style.display = 'none';; //This will be delayed for one second
+    }, 300);
+  });
